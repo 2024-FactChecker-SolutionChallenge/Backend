61a3226f912e0db8bd5d0f0179f61c3eda9465d7
ahyeon-github <ahyeon31@sookmyung.ac.kr>
2024-02-04 21:19:45 +0900

    [feat]Email authentication with SMTP

--- src/main/java/com/solutionchallenge/factchecker/Member/controller/MemberController.java
+++ src/main/java/com/solutionchallenge/factchecker/Member/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.solutionchallenge.factchecker.Member.controller;
+import com.solutionchallenge.factchecker.Member.dto.MailConfirmDto;
 import com.solutionchallenge.factchecker.Member.dto.request.MemberSignupRequest;
 import com.solutionchallenge.factchecker.Member.dto.response.MemberSignupResponse;
+import com.solutionchallenge.factchecker.Member.service.EmailService;
 import com.solutionchallenge.factchecker.Member.service.MemberService;
 import com.solutionchallenge.factchecker.global.dto.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,12 +11,34 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/auth")
 public class MemberController {
+
+    private final MemberService userService;
+    private final EmailService emailService;
+
     @Autowired
-    private MemberService userService;
+    public MemberController(MemberService userService, EmailService emailService) {
+        this.userService = userService;
+        this.emailService = emailService;
+    }
+    @Operation(summary = "이메일 인증", description = "이메일 인증번호 (ePw) 를 입력받은 메일로 전송합니다.")
+    @PostMapping("/confirm")
+    public ResponseEntity<?> emailConfirm(@RequestBody MailConfirmDto dto) {
+        try {
+            // 이메일을 보내고 인증 코드를 생성
+            String ePw = emailService.sendEmailAndGenerateCode(dto.getEmail());
+            Map<String, String> confirmResult = new HashMap<>();
+            confirmResult.put("AuthenticationCode", ePw);
+            return ResponseEntity.ok(confirmResult);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("확인 이메일 전송 중 오류가 발생했습니다.");
+        }
+    }
 
     @Operation(summary = "사용자 회원가입하기")
     @PostMapping("/signup")
---
+++ src/main/java/com/solutionchallenge/factchecker/Member/dto/MailConfirmDto.java
@@ -0,0 +1,7 @@
+package com.solutionchallenge.factchecker.Member.dto;
+import lombok.Data;
+@Data
+public class MailConfirmDto {
+
+    public String email;
+}
