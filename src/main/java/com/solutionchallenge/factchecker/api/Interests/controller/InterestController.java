b1121fe1a88f60c4b6472a37bf9424c2dc1ce9f1
Chaeyeon Park <chay322p@gmail.com>
2024-02-18 02:31:55 +0900

    [feat] 관심기사 DB에 저장하기

--- src/main/java/com/solutionchallenge/factchecker/api/Interests/controller/InterestController.java
+++ src/main/java/com/solutionchallenge/factchecker/api/Interests/controller/InterestController.java
@@ -6,6 +6,14 @@ import com.solutionchallenge.factchecker.api.Interests.dto.response.SelectedInte
 import com.solutionchallenge.factchecker.api.Interests.entity.Interest;
 import com.solutionchallenge.factchecker.api.Interests.service.InterestService;
 import com.solutionchallenge.factchecker.api.Member.entity.UserDetailsImpl;
+import com.solutionchallenge.factchecker.api.Youtube.dto.response.YoutubeResponseDto;
+import com.solutionchallenge.factchecker.api.Youtube.dto.response.YoutubeSuccessDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +21,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+@Tag(name= "Interest Controller ", description = "관심기사 API")
 @RestController
 @RequestMapping("/api/interests")
 @RequiredArgsConstructor
@@ -32,6 +42,10 @@ public class InterestController {
         SelectedInterestsResponseDto responseDto = interestService.setSelectedInterests(memberId, requestDto);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
+    @Operation(summary = "홈(메인) 화면 - ML 서버 값 DB저장 ", description = "ml서버에 요청을 보내고 받은 응답을 DB에 저장합니다.(차후 한시간에 한번씩 자동으로 실행되도록 변경 필요)",
+            responses = {@ApiResponse(responseCode = "200", description = "관심기사 목록이 성공적으로 DB에 저장되었습니다.",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = InterestArticleResponseDto.class))))
+            })
     @GetMapping("/getarticle")
     public ResponseEntity<List<InterestArticleResponseDto>> getInterestArticles(
             @RequestHeader(name = "ACCESS_TOKEN", required = false) String ACCESS_TOKEN,
@@ -39,8 +53,7 @@ public class InterestController {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         UserDetailsImpl userDetails = (UserDetailsImpl) principal;
 
-
-        List<InterestArticleResponseDto> dtos = interestService.getInterestArticles(userDetails.getUsername());
+        List<InterestArticleResponseDto> dtos = interestService.getArticles(userDetails.getUsername());
         return ResponseEntity.ok(dtos);
     }
 
--- src/main/java/com/solutionchallenge/factchecker/api/Interests/service/InterestService.java
+++ src/main/java/com/solutionchallenge/factchecker/api/Interests/service/InterestService.java
@@ -1,4 +1,5 @@
 package com.solutionchallenge.factchecker.api.Interests.service;
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -16,6 +17,7 @@ import com.solutionchallenge.factchecker.api.Member.repository.MemberRepository;
 import com.solutionchallenge.factchecker.global.exception.CustomException;
 import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
@@ -82,94 +84,60 @@ public class InterestService {
                 .selectedInterests(updatedSelectedInterests)
                 .build();
     }
+    public List<InterestArticleResponseDto> getArticles(String memberId) {
+        //1. ML에서 전체 기사 데이터 받아오기 MLResponseDTO로 매핑해서 가지고 있기./ DTo를 Entity로 매핑해서 DB에 저장하기
+        saveInterestsFromMLResponse();
+        // DB에서 모든 Interest 엔티티를 조회
+        List<Interest> interests = interestRepository.findAll();
 
-    private Mono<MLResponseDto> sendUrlToMlServer(String member_id, String url) {
-        return webClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/interests")
-                        .queryParam("url", url)
-                        .build())
-                .exchangeToMono(response -> {
-                    if (response.statusCode().is2xxSuccessful()) {
-                        // 200번대 응답 처리
-                        return response.bodyToMono(String.class)
-                                .flatMap(body -> {
-                                    try {
-                                        return handleMlServerResponse(member_id, url, body);
-                                    } catch (JsonProcessingException e) {
-                                        return Mono.error(new RuntimeException(e));
-                                    }
-                                });
-                    } else if (response.statusCode().is4xxClientError()) {
-                        // 400번대 에러 처리, 예외 발생
-                        return Mono.error(new CustomException("Client error, incorrect request"));
-                    } else if (response.statusCode().is5xxServerError()) {
-                        // 500번대 에러 처리, 재시도를 위해 에러 발생
-                        return Mono.error(new CustomException("Server error, retrying..."));
-                    } else {
-                        // 그 외 상태 코드 처리, 예외 발생
-                        return Mono.error(new CustomException("Unexpected response status: " + response.statusCode()));
-                    }
-                })
-                .retryWhen(Retry.max(5)
-                        .filter(throwable -> throwable instanceof CustomException && throwable.getMessage().contains("retrying")))
-                .onErrorResume(e -> {
-                    log.error("After retries or timeout, processing failed: {}", e.getMessage());
-                    return Mono.error(new CustomException("재시도 횟수를 다 사용했습니다. ML 서버의 트래픽이 너무 많거나 응답 시간이 너무 깁니다. 재요청해주세요"));
-                });
+        // 조회된 Interest 엔티티 리스트를 InterestArticleResponseDto 리스트로 변환
+        List<InterestArticleResponseDto> dtos = interests.stream()
+                .map(this::convertEntityToDto)
+                .collect(Collectors.toList());
+        //FE로 반환
+        return dtos;
+
     }
 
-    private Mono<MLResponseDto> handleMlServerResponse(String member_id, String url, String response) throws JsonProcessingException {
-        return Mono.fromCallable(() -> {
-            Member member = memberRepository.findMemberById(member_id).orElseThrow(() -> new CustomException("User not found"));
+    @Transactional
+    public void saveInterestsFromMLResponse() {
+        // fetchDataFromMLServer()가 Mono<List<MLResponseDto>> 대신 List<MLResponseDto>를 반환한다고 가정
+        List<MLResponseDto> dtos = fetchDataFromMLServer().block(); // 리액티브 타입을 동기적으로 처리
 
-            Timestamp now = new Timestamp(System.currentTimeMillis());
-
-            // JSON 응답을 MLResponseDto 객체로 변환
-            ObjectMapper objectMapper = new ObjectMapper();
-            MLResponseDto mlResponse = objectMapper.readValue(response, MLResponseDto.class);
-
-            return mlResponse; // MLResponseDto 반환
-        });
-    }
-
-    public List<InterestArticleResponseDto> getInterestArticles(String memberId) {
-        // 회원 ID를 사용하여 선택한 키워드를 가져옵니다.
-        List<String> selectedInterests = getSelectedInterests(memberId);
-
-        List<InterestArticleResponseDto> interestArticleResponseDtos = new ArrayList<>();
-
-        // 각 키워드에 대해 ML 서버로 요청을 보내어 기사 목록을 가져옵니다.
-        for (String keyword : selectedInterests) {
-            Mono<MLResponseDto> mlResponseMono = sendUrlToMlServer(memberId, keyword);
-            mlResponseMono.subscribe(mlResponse -> {
-                // ML 서버에서 가져온 기사 목록을 InterestArticleResponseDto로 변환하여 리스트에 추가합니다.
-                InterestArticleResponseDto dto = new InterestArticleResponseDto();
-                dto.setTitle(mlResponse.getTitle());
-                dto.setArticle(mlResponse.getArticle());
-                dto.setDate(mlResponse.getDate());
-                dto.setCredibility(mlResponse.getCredibility());
-                dto.setSection(mlResponse.getSection());
-                interestArticleResponseDtos.add(dto);
-
-                // 가져온 기사 목록을 interest_article 테이블에 저장합니다.
-                saveInterestArticle(dto);
-            });
+        for (MLResponseDto dto : dtos) {
+            Interest interest = convertDtoToEntity(dto);
+            interestRepository.save(interest); // 변환된 엔티티를 데이터베이스에 저장
         }
-
-        return interestArticleResponseDtos;
     }
 
-    public void saveInterestArticle(InterestArticleResponseDto interestArticleResponseDto) {
+    private Mono<List<MLResponseDto>> fetchDataFromMLServer() {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/interests").build())
+                .retrieve() // exchangeToMono 대신 retrieve 사용
+                .bodyToMono(new ParameterizedTypeReference<List<MLResponseDto>>() {});
+    }
+    private Interest convertDtoToEntity(MLResponseDto dto) {
         Interest interest = new Interest();
-        interest.setTitle(interestArticleResponseDto.getTitle());
-        interest.setArticle(interestArticleResponseDto.getArticle());
-        interest.setDate(interestArticleResponseDto.getDate());
-        interest.setCredibility(interestArticleResponseDto.getCredibility());
-        interest.setSection(interestArticleResponseDto.getSection());
+        interest.setTitle(dto.getTitle());
+        interest.setArticle(dto.getArticle());
+        interest.setDate(dto.getDate());
+        interest.setCredibility(dto.getCredibility());
+        interest.setSection(dto.getSection());
 
-        interestRepository.save(interest);
+        return interest;
     }
 
+    private InterestArticleResponseDto convertEntityToDto(Interest interest) {
+        // Interest 엔티티를 InterestArticleResponseDto로 변환하는 로직
+        InterestArticleResponseDto dto = new InterestArticleResponseDto();
+        dto.setId(interest.getId());
+        dto.setTitle(interest.getTitle());
+        dto.setArticle(interest.getArticle());
+        dto.setDate(interest.getDate());
+        dto.setCredibility(interest.getCredibility());
+        dto.setSection(interest.getSection());
+        return dto;
+    }
 
 //    @Transactional
 //    public List<InterestArticleResponseDto> getInterestArticles() {
