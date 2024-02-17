0c90d07373ba016605f4cd5a24da1d91ec8a9429
chay3 <chay322p@gmail.com>
2024-02-17 16:36:53 +0900

    [Refactor] change youtube entity creation timing

--- src/main/java/com/solutionchallenge/factchecker/api/Youtube/dto/response/YoutubeResponseDto.java
+++ src/main/java/com/solutionchallenge/factchecker/api/Youtube/dto/response/YoutubeResponseDto.java
@@ -3,6 +3,8 @@ package com.solutionchallenge.factchecker.api.Youtube.dto.response;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
 import java.util.List;
 
 @Getter
@@ -11,6 +13,8 @@ public class YoutubeResponseDto {
     private Long id;
     private String title;
     private String url;
+    private final boolean show= false;
+    private final boolean loadingStatus=false;
     private List<RelatedNewsDto> relatedNews;
 
     public YoutubeResponseDto(Long id, String title, String url, List<RelatedNewsDto> relatedNews) {
