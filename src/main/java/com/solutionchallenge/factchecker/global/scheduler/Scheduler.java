4800e61742efb52897ad184bf91d9d8885360efb
Chaeyeon Park <chay322p@gmail.com>
2024-02-18 20:06:28 +0900

    [refactor] 관심기사업데이트 3시간마다 실행

--- src/main/java/com/solutionchallenge/factchecker/global/scheduler/Scheduler.java
+++ src/main/java/com/solutionchallenge/factchecker/global/scheduler/Scheduler.java
@@ -1,7 +1,6 @@
 package com.solutionchallenge.factchecker.global.scheduler;
 
 import com.solutionchallenge.factchecker.api.Interests.service.InterestService;
-import com.solutionchallenge.factchecker.api.Learn.service.LearnService;
 import com.solutionchallenge.factchecker.api.Member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,7 +17,6 @@ public class Scheduler {
     // 매주 월요일 자정에 weekNews와 dailyScore 초기화
     @Scheduled(cron = "0 0 0 * * MON", zone = "Asia/Seoul")
     public void resetWeeklyData() {
-        // 여기에 weekNews와 dailyScore 초기화 로직을 구현하세요.
         log.info("주간 뉴스 조회 및 데일리 퀴즈 점수 초기화 작업을 시작합니다.");
         memberService.resetWeeklyData();
     }
@@ -26,13 +24,12 @@ public class Scheduler {
     // 매일 자정에 left_opportunities 초기화
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     public void resetDailyOpportunities() {
-        // 여기에 left_opportunities 초기화 로직을 구현하세요.
         log.info("데일리 퀴즈 도전 기회 초기화 작업을 시작합니다.");
         memberService.resetDailyOpportunities();
     }
 
     // 정시에 3시간마다 실행
-    @Scheduled(cron = "0 */5 * * * *", zone = "Asia/Seoul")//(cron = "0 0 */3 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 0 */3 * * *", zone = "Asia/Seoul")
     public void updateArticles() {
         log.info("기사 업데이트 작업을 시작합니다.");
         interestService.updateArticles();
